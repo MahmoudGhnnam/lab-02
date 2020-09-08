bed60348136d4dbd18313872427c1f4de483a07d
MahmoudGhnnam <mahmou999@gmail.com>
2020-09-08 09:49:10 +0300

    remove spaces in code

--- js/app.js
+++ js/app.js
@@ -8,110 +8,110 @@ var userAnswre = prompt('is my name khaled? [yes/no]'.toLocaleLowerCase());
 console.log('user answer ' + userAnswre)
 
 switch (userAnswre.toLocaleLowerCase()) {
-    case 'yes':
-        console.log('user say\'s' + userAnswre);
-        alert('No! it\'s wrong');
-        break;
+	case 'yes':
+		console.log('user say\'s' + userAnswre);
+		alert('No! it\'s wrong');
+		break;
 
-    case 'no':
-        console.log('user say\'s' + userAnswre);
-        alert('it\'s correct');
-        break;
+	case 'no':
+		console.log('user say\'s' + userAnswre);
+		alert('it\'s correct');
+		break;
 
-    default:
-        alert('you should answre with yes or no')
-        break;
+	default:
+		alert('you should answre with yes or no')
+		break;
 }
 
 var userAnswre = prompt('do i have a youtube channel? [yes/no]'.toLocaleLowerCase());
 console.log('user answer ' + userAnswre)
 
 switch (userAnswre.toLocaleLowerCase()) {
-    case 'no':
-        console.log('user say\'s' + userAnswre);
-        alert('No! it\'s wrong');
-        break;
+	case 'no':
+		console.log('user say\'s' + userAnswre);
+		alert('No! it\'s wrong');
+		break;
 
-    case 'yes':
-        console.log('user say\'s' + userAnswre);
-        alert('it\'s correct');
-        break;
+	case 'yes':
+		console.log('user say\'s' + userAnswre);
+		alert('it\'s correct');
+		break;
 
-    default:
-        alert('you should answre with yes or no')
-        break;
+	default:
+		alert('you should answre with yes or no')
+		break;
 }
 
 var userAnswre = prompt('Am I from Syria? [yes/no]'.toLocaleLowerCase());
 console.log('user answer ' + userAnswre)
 
 switch (userAnswre.toLocaleLowerCase()) {
-    case 'no':
-        console.log('user say\'s' + userAnswre);
-        alert('No! it\'s wrong');
-        break;
+	case 'no':
+		console.log('user say\'s' + userAnswre);
+		alert('No! it\'s wrong');
+		break;
 
-    case 'yes':
-        console.log('user say\'s' + userAnswre);
-        alert('it\'s correct');
-        break;
+	case 'yes':
+		console.log('user say\'s' + userAnswre);
+		alert('it\'s correct');
+		break;
 
-    default:
-        alert('you should answre with yes or no')
-        break;
+	default:
+		alert('you should answre with yes or no')
+		break;
 }
 
 var userAnswre = prompt('Did I graduate from LTUC? [yes/no]'.toLocaleLowerCase());
 console.log('user answer ' + userAnswre)
 
 switch (userAnswre.toLocaleLowerCase()) {
-    case 'no':
-        console.log('user say\'s' + userAnswre);
-        alert('No! it\'s wrong');
-        break;
+	case 'no':
+		console.log('user say\'s' + userAnswre);
+		alert('No! it\'s wrong');
+		break;
 
-    case 'yes':
-        console.log('user say\'s' + userAnswre);
-        alert('it\'s correct');
-        break;
+	case 'yes':
+		console.log('user say\'s' + userAnswre);
+		alert('it\'s correct');
+		break;
 
-    default:
-        alert('you should answre with yes or no')
-        break;
+	default:
+		alert('you should answre with yes or no')
+		break;
 }
 
 var userAnswre = prompt('my channel on youtube named Mahmoud? [yes/no]');
 console.log('user answer ' + userAnswre)
 
 if (userAnswre.toLocaleLowerCase() == 'yes') {
-    console.log('user say\'s' + userAnswre);
-    alert('No! it\'s wrong');
+	console.log('user say\'s' + userAnswre);
+	alert('No! it\'s wrong');
 }
 
 else if (userAnswre == 'no') {
-    console.log('user say\'s' + userAnswre);
-    alert('it\'s correct');
+	console.log('user say\'s' + userAnswre);
+	alert('it\'s correct');
 }
 
 else {
-    alert('you should answre with yes or no')
+	alert('you should answre with yes or no')
 }
 
 var userAnswre = prompt('Have i worked before? [yes/no]'.toLocaleLowerCase());
 console.log('user answer ' + userAnswre)
 
 if (userAnswre === 'no'.toLocaleLowerCase()) {
-    console.log('user say\'s' + userAnswre);
-    alert('No! it\'s wrong');
+	console.log('user say\'s' + userAnswre);
+	alert('No! it\'s wrong');
 }
 
 else if (userAnswre === 'yes') {
-    console.log('user say\'s' + userAnswre);
-    alert('it\'s correct');
+	console.log('user say\'s' + userAnswre);
+	alert('it\'s correct');
 }
 
 else {
-    alert('you should answre with yes or no')
+	alert('you should answre with yes or no')
 }
 
 alert('this is my quiz to my websit and now you\'r welcome  ' + userName)
